8dc55cc5e2ca17bccb80548873c379c2fa2a119e
lulb8 <bonneville@et.esiea.fr>
2020-02-26 15:31:40 +0100

    Implementation of every moves (f, b, l, r)

--- src/main/java/com/esiea/tp4A/domain/MarsRoverImpl.java
+++ src/main/java/com/esiea/tp4A/domain/MarsRoverImpl.java
@@ -13,11 +13,49 @@ public class MarsRoverImpl implements MarsRover {
         if (command.isEmpty()) {
             return position;
         }
-        char singleCommand = command.charAt(0);
-        if ('f' == singleCommand) {
-            position = Position.of(position.getX(), position.getY() + 1, position.getDirection());
-        } else if ('b' == singleCommand) {
-            position = Position.of(position.getX(), position.getY() - 1, position.getDirection());
+        for (int count = 0; count < command.length(); count++) {
+            char singleCommand = command.charAt(count);
+            if ('f' == singleCommand) {
+                if (position.getDirection() == Direction.NORTH) {
+                    position = Position.of(position.getX(), position.getY() + 1, Direction.NORTH);
+                } else if (position.getDirection() == Direction.WEST) {
+                    position = Position.of(position.getX() - 1, position.getY(), Direction.WEST);
+                } else if (position.getDirection() == Direction.SOUTH) {
+                    position = Position.of(position.getX(), position.getY() - 1, Direction.SOUTH);
+                } else if (position.getDirection() == Direction.EAST) {
+                    position = Position.of(position.getX() + 1, position.getY(), Direction.EAST);
+                }
+            } else if ('b' == singleCommand) {
+                if (position.getDirection() == Direction.NORTH) {
+                    position = Position.of(position.getX(), position.getY() - 1, Direction.NORTH);
+                } else if (position.getDirection() == Direction.WEST) {
+                    position = Position.of(position.getX() + 1, position.getY(), Direction.WEST);
+                } else if (position.getDirection() == Direction.SOUTH) {
+                    position = Position.of(position.getX(), position.getY() + 1, Direction.SOUTH);
+                } else if (position.getDirection() == Direction.EAST) {
+                    position = Position.of(position.getX() - 1, position.getY(), Direction.EAST);
+                }
+            } else if ('l' == singleCommand) {
+                if (position.getDirection() == Direction.NORTH) {
+                    position = Position.of(position.getX(), position.getY(), Direction.WEST);
+                } else if (position.getDirection() == Direction.WEST) {
+                    position = Position.of(position.getX(), position.getY(), Direction.SOUTH);
+                } else if (position.getDirection() == Direction.SOUTH) {
+                    position = Position.of(position.getX(), position.getY(), Direction.EAST);
+                } else if (position.getDirection() == Direction.EAST) {
+                    position = Position.of(position.getX(), position.getY(), Direction.NORTH);
+                }
+            } else if ('r' == singleCommand) {
+                if (position.getDirection() == Direction.NORTH) {
+                    position = Position.of(position.getX(), position.getY(), Direction.EAST);
+                } else if (position.getDirection() == Direction.EAST) {
+                    position = Position.of(position.getX(), position.getY(), Direction.SOUTH);
+                } else if (position.getDirection() == Direction.SOUTH) {
+                    position = Position.of(position.getX(), position.getY(), Direction.WEST);
+                } else if (position.getDirection() == Direction.WEST) {
+                    position = Position.of(position.getX(), position.getY(), Direction.NORTH);
+                }
+            }
         }
         return position;
     }
--- src/test/java/com/esiea/tp4A/MarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/MarsRoverTest.java
@@ -6,6 +6,10 @@ import com.esiea.tp4A.domain.MarsRoverImpl;
 import com.esiea.tp4A.domain.Position;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MarsRoverTest {
 
@@ -31,5 +35,35 @@ public class MarsRoverTest {
         Position newPosition = marsRover.move("b");
 
         Assertions.assertThat(newPosition).isEqualTo(Position.of(0, -1, Direction.NORTH));
+    }
+
+    @Test
+    void move_left() {
+        MarsRover marsRover = new MarsRoverImpl(0, 0, Direction.NORTH);
+        Position newPosition = marsRover.move("l");
+
+        Assertions.assertThat(newPosition).isEqualTo(Position.of(0, 0, Direction.WEST));
+    }
+
+    @Test
+    void move_right() {
+        MarsRover marsRover = new MarsRoverImpl(0, 0, Direction.NORTH);
+        Position newPosition = marsRover.move("r");
+
+        Assertions.assertThat(newPosition).isEqualTo(Position.of(0, 0, Direction.EAST));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "'f', 0, 1, NORTH",
+        "'l', 0, 0, WEST",
+        "'fflb', 1, 2, WEST"
+    })
+    void move_from_center(String command, int expectedX, int expectedY, Direction expectedDirection) {
+        MarsRover marsRover = new MarsRoverImpl(0, 0, Direction.NORTH);
+        Position newPosition = marsRover.move(command);
+
+        Assertions.assertThat(newPosition).isEqualTo(Position.of(expectedX, expectedY, expectedDirection));
+
     }
 }
