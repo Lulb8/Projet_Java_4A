185da6336a9e288fc62f182f15abe05a46ec98c9
lulb8 <bonneville@et.esiea.fr>
2020-03-18 22:32:45 +0100

    Creation of methods and code sharing for movements and check borders

--- src/main/java/com/esiea/tp4A/domain/MarsRoverImpl.java
+++ src/main/java/com/esiea/tp4A/domain/MarsRoverImpl.java
@@ -1,7 +1,6 @@
 package com.esiea.tp4A.domain;
 
 public class MarsRoverImpl implements MarsRover {
-
     Position position;
 
     public MarsRoverImpl(int x, int y, Direction direction) {
@@ -10,18 +9,10 @@ public class MarsRoverImpl implements MarsRover {
 
     @Override
     public Position move(String command) {
-        int x= position.getX();
-        int y = position.getY();
-        Direction direction = position.getDirection();
-
-        int originalX= position.getX();
-        int originalY = position.getY();
-        Direction originalDirection = position.getDirection();
-
-        if (command.isEmpty()) {
+        int x = position.getX(), originalX = position.getX(), y = position.getY(), originalY = position.getY();
+        Direction direction = position.getDirection(), originalDirection = position.getDirection();
+        if (command.isEmpty())
             return position;
-        }
-
         for (int count = 0; count < command.length(); count++) {
             char singleCommand = command.charAt(count);
             switch (singleCommand) {
@@ -29,18 +20,22 @@ public class MarsRoverImpl implements MarsRover {
                     Object[] newPositionF = move_rover_forward(x, y, direction);
                     x = (int) newPositionF[0];
                     y = (int) newPositionF[1];
-                    position = Position.of(x, y, direction); break;
+                    position = Position.of(x, y, direction);
+                    break;
                 case 'b':
                     Object[] newPositionB = move_rover_backward(x, y, direction);
                     x = (int) newPositionB[0];
                     y = (int) newPositionB[1];
-                    position = Position.of(x, y, direction); break;
+                    position = Position.of(x, y, direction);
+                    break;
                 case 'l':
                     direction = move_rover_left(direction);
-                    position = Position.of(x, y, direction); break;
+                    position = Position.of(x, y, direction);
+                    break;
                 case 'r':
                     direction = move_rover_right(direction);
-                    position = Position.of(x, y, direction); break;
+                    position = Position.of(x, y, direction);
+                    break;
                 default:
                     return position = Position.of(originalX, originalY, originalDirection);
             }
@@ -48,89 +43,57 @@ public class MarsRoverImpl implements MarsRover {
         return position;
     }
 
-    public Object[] move_rover_forward(int x, int y, Direction direction){
-        int newX = x;
-        int newY = y;
-        switch (direction){
-            case NORTH:
-                if (y < 50) {
-                    newY++;
-                } else if (y == 50) {
-                    newY = -49;
-                } break;
-            case EAST:
-                if (x < 50) {
-                    newX++;
-                } else if (x == 50) {
-                    newX = -49;
-                } break;
-            case SOUTH:
-                if (y > -49) {
-                    newY--;
-                } else if (y == -49) {
-                    newY = 50;
-                } break;
-            case WEST:
-                if (x > -49) {
-                    newX--;
-                } else if (x == -49) {
-                    newX = 50;
-                } break;
-        }
-        return new Object[]{newX, newY};
+    public Object[] move_rover_forward(int x, int y, Direction direction) {
+        switch (direction) {
+            case NORTH: y = north_east_border(y); break;
+            case EAST: x = north_east_border(x); break;
+            case SOUTH: y = south_west_border(y); break;
+            case WEST: x = south_west_border(x); break;
+        } return new Object[]{x, y};
     }
 
-    public Object[] move_rover_backward(int x, int y, Direction direction){
-        int newX = x;
-        int newY = y;
-        switch (direction){
-            case NORTH:
-                if (y > -49) {
-                    newY--;
-                } else if (y == -49) {
-                    newY = 50;
-                } break;
-            case EAST:
-                if (x > -49) {
-                    newX--;
-                } else if (x == -49) {
-                    newX = 50;
-                } break;
-            case SOUTH:
-                if (y < 50) {
-                    newY++;
-                } else if (y == 50) {
-                    newY = -49;
-                } break;
-            case WEST:
-                if (x < 50) {
-                    newX++;
-                } else if (x == 50) {
-                    newX = -49;
-                } break;
-        }
-        return new Object[]{newX, newY};
+    public Object[] move_rover_backward(int x, int y, Direction direction) {
+        switch (direction) {
+            case NORTH: y = south_west_border(y); break;
+            case EAST: x = south_west_border(x); break;
+            case SOUTH: y = north_east_border(y); break;
+            case WEST: x = north_east_border(x); break;
+        } return new Object[]{x, y};
     }
 
-    public Direction move_rover_right(Direction direction){
+    public Direction move_rover_right(Direction direction) {
         Direction newDirection = direction;
         switch (direction) {
             case NORTH: newDirection = Direction.EAST; break;
             case WEST: newDirection = Direction.NORTH; break;
             case SOUTH: newDirection = Direction.WEST; break;
             case EAST: newDirection = Direction.SOUTH; break;
-        }
-        return newDirection;
+        } return newDirection;
     }
 
-    public Direction move_rover_left(Direction direction){
+    public Direction move_rover_left(Direction direction) {
         Direction newDirection = direction;
         switch (direction) {
             case NORTH: newDirection = Direction.WEST; break;
             case WEST: newDirection = Direction.SOUTH; break;
             case SOUTH: newDirection = Direction.EAST; break;
             case EAST: newDirection = Direction.NORTH; break;
-        }
-        return newDirection;
+        } return newDirection;
+    }
+
+    public int north_east_border(int p) {
+        if (p < 50)
+            p++;
+        else if (p == 50)
+            p = -49;
+        return p;
+    }
+
+    public int south_west_border(int p) {
+        if (p > -49)
+            p--;
+        else if (p == -49)
+            p = 50;
+        return p;
     }
 }
