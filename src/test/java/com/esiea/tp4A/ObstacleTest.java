e5bc373808ba80a8b2893e5c5ab50872f10f7ecf
Lens <lenny.daho@gmail.com>
2020-04-11 01:47:56 +0200

    Test obtacle with move of marsrover

--- src/test/java/com/esiea/tp4A/ObstacleTest.java
+++ src/test/java/com/esiea/tp4A/ObstacleTest.java
@@ -7,6 +7,8 @@ import com.esiea.tp4A.domain.MarsRover;
 import com.esiea.tp4A.domain.MarsRoverImpl;
 import com.esiea.tp4A.domain.PlanetMapImpl;
 import com.esiea.tp4A.domain.Position;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ObstacleTest {
@@ -14,9 +16,9 @@ public class ObstacleTest {
     @Test
     void detect_obstacles() {
         MarsRover marsrover = new MarsRoverImpl(0, 0, Direction.NORTH);
-        Position pos1 = Position.of(0, 1, Direction.NORTH);
-        Set<Position> hash_Set = new HashSet<Position>();
-        hash_Set.add(pos1);
-        PlanetMapImpl.obstaclePositions();
+        Position newposition = marsrover.move("fflb");
+
+        if (newposition == PlanetMapImpl.obstaclePositions())
+            Assertions.assertThat(newposition).isEqualTo(Position.of(1, 0, Direction.WEST));
     }
 }
